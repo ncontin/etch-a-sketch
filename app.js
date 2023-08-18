6bf8cd17b0e0d27a5e7240576a8b69cecd55d6a4
Nicolò Contin <95549154+nicolo-contin@users.noreply.github.com>
2023-08-18 16:07:11 +0200

    add black color

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const container = document.querySelector("#container");
 const btn = document.querySelector("#btn");
+const color = document.querySelector(".colorBtn");
+const black = document.querySelector("#blackBtn");
+const clear = document.querySelector(".clearBtn");
+
 let dynamicValue = 16;
 
 function createGrid(x) {
@@ -18,15 +22,46 @@ function createGrid(x) {
 
 createGrid(16);
 
+let isBlack = true;
+let isColor = false;
+
+black.addEventListener("click", () => {
+  isBlack = true;
+  console.log("black enabled");
+});
+
+
+
 function addHovering() {
   const grids = document.querySelectorAll(".grid");
-  grids.forEach((grid) => {
-    grid.addEventListener("mouseover", (e) => {
-      let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-      e.target.style.backgroundColor = `#${randomColor}`;
+  if (isBlack) {
+    grids.forEach((grid) => {
+      grid.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor = `black`;
+        grid.classList.add("active");
+      });
     });
-  });
+  } else {
+    grids.forEach((grid) => {
+      grid.addEventListener("mouseover", (e) => {
+        let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        e.target.style.backgroundColor = `#${randomColor}`;
+        grid.classList.add("active");
+      });
+    });
+  }
 }
+
+// function addTransparency() {
+//   const active = document.querySelectorAll(".active");
+//   active.forEach((square) => {
+//     square.addEventListener("mouseover", (e) => {
+//       let
+//       e.target.style.backgroundColor = `#${randomColor}`;
+//       grid.classList.add("active");
+//     });
+//   });
+// }
 
 addHovering();
 
@@ -40,10 +75,14 @@ function clearGrid() {
 btn.addEventListener("click", () => {
   const numberOfSquares = Number(window.prompt("enter the number of square per side, MAX 100", ""));
 
-  if (Number.isInteger(numberOfSquares) && parseInt(numberOfSquares) <= 100) {
-    dynamicValue = numberOfSquares;
+  if (numberOfSquares && numberOfSquares <= 100 && numberOfSquares > 0) {
     clearGrid();
     createGrid(numberOfSquares);
+    addHovering();
+  } else {
+    console.log("prompt cancelled");
+    clearGrid();
+    createGrid(16);
     addHovering();
   }
 });
