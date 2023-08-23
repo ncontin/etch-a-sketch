4308b8bc957a3a87e0ae419f63e4ac1cec715699
Nicolo Contin <95549154+ncontin@users.noreply.github.com>
2023-08-23 19:05:24 +0200

    user can now click on the board to start the draw

--- app.js
+++ app.js
@@ -10,99 +10,128 @@ let dynamicValue = 64;
 let isBlack = true;
 let isColor = false;
 let isErased = false;
+let clicked = false;
+let hoverFunction;
+
 //Functions
 
 function createGrid(x) {
-  for (let rows = 0; rows < x; rows++) {
-    for (let cols = 0; cols < x; cols++) {
-      const div = document.createElement("div");
-      div.classList.add("grid");
-      board.appendChild(div);
+    for (let rows = 0; rows < x; rows++) {
+        for (let cols = 0; cols < x; cols++) {
+            const div = document.createElement("div");
+            div.classList.add("grid");
+            board.appendChild(div);
+        }
     }
-  }
-  const grids = document.querySelectorAll(".grid");
-  grids.forEach((grid) => {
-    grid.style.flexBasis = `calc(100% / ${dynamicValue})`;
-  });
+    const grids = document.querySelectorAll(".grid");
+    grids.forEach((grid) => {
+        grid.style.flexBasis = `calc(100% / ${dynamicValue})`;
+    });
 }
 
 createGrid(64);
 
+function click() {
+    const grids = document.querySelectorAll(".grid");
+    grids.forEach((grid) => {
+        grid.addEventListener("click", () => {
+            clicked = !clicked;
+            console.log("clicked", clicked);
+            addHovering();
+        });
+    });
+}
+click();
+
 function addHovering() {
-  const grids = document.querySelectorAll(".grid");
-  if (isBlack) {
-    grids.forEach((grid) => {
-      grid.addEventListener("mouseover", (e) => {
-        e.target.style.backgroundColor = `black`;
-      });
-    });
-  } else if (isColor) {
-    grids.forEach((grid) => {
-      grid.addEventListener("mouseover", (e) => {
-        let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-        e.target.style.backgroundColor = `#${randomColor}`;
-      });
-    });
-  } else if (isErased) {
-    grids.forEach((grid) => {
-      grid.addEventListener("mouseover", (e) => {
-        e.target.style.backgroundColor = ``;
-      });
-    });
-  }
+    const grids = document.querySelectorAll(".grid");
+
+    // Remove previous event listeners
+    if (hoverFunction) {
+        grids.forEach((grid) => {
+            grid.removeEventListener("mouseover", hoverFunction);
+        });
+    }
+
+    if (clicked === true) {
+        if (isBlack) {
+            hoverFunction = (e) => {
+                e.target.style.backgroundColor = `black`;
+            };
+        } else if (isColor) {
+            hoverFunction = (e) => {
+                let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+                e.target.style.backgroundColor = `#${randomColor}`;
+            };
+        } else if (isErased) {
+            hoverFunction = (e) => {
+                e.target.style.backgroundColor = ``;
+            };
+        }
+
+        // Add new event listeners
+        grids.forEach((grid) => {
+            grid.addEventListener("mouseover", hoverFunction);
+        });
+    }
 }
 
 addHovering();
 
 function clearGrid() {
-  const grids = document.querySelectorAll(".grid");
-  grids.forEach((grid) => {
-    grid.style.backgroundColor = "";
-  });
+    clicked = false;
+    const grids = document.querySelectorAll(".grid");
+    grids.forEach((grid) => {
+        grid.style.backgroundColor = "";
+    });
+    addHovering();
 }
 
 function removeGrid() {
-  const grids = document.querySelectorAll(".grid");
-  grids.forEach((grid) => {
-    grid.remove();
-  });
+    const grids = document.querySelectorAll(".grid");
+    grids.forEach((grid) => {
+        grid.remove();
+    });
 }
 
 //Event Listeners
 
 black.addEventListener("click", () => {
-  isBlack = true;
-  isColor = false;
-  isErased = false;
-  addHovering();
+    isBlack = true;
+    isColor = false;
+    isErased = false;
+    clicked = false;
+    addHovering();
 });
 color.addEventListener("click", () => {
-  isColor = true;
-  isBlack = false;
-  isErased = false;
-  addHovering();
+    isColor = true;
+    isBlack = false;
+    isErased = false;
+    clicked = false;
+    addHovering();
 });
 eraser.addEventListener("click", () => {
-  isErased = true;
-  isColor = false;
-  isBlack = false;
-  addHovering();
-  console.log("eraser enabled");
+    isErased = true;
+    isColor = false;
+    isBlack = false;
+    clicked = false;
+    addHovering();
+    console.log("eraser enabled");
 });
 
 clear.addEventListener("click", () => {
-  clearGrid();
+    clearGrid();
 });
 
 btn.addEventListener("click", () => {
-  const numberOfSquares = Number(window.prompt("Enter the number of squares per side, MAX 100", ""));
+    const numberOfSquares = Number(window.prompt("Enter the number of squares per side, MAX 100", ""));
 
-  if (numberOfSquares && numberOfSquares <= 100 && numberOfSquares > 0) {
-    dynamicValue = numberOfSquares;
-    removeGrid();
-    createGrid(numberOfSquares);
-    addHovering();
-  } else {
-    return;
-  }
+    if (numberOfSquares && numberOfSquares <= 100 && numberOfSquares > 0) {
+        dynamicValue = numberOfSquares;
+        removeGrid();
+        createGrid(numberOfSquares);
+        addHovering();
+    } else {
+        return;
+    }
 });
